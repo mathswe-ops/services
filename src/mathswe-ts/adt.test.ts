962d75cc18843ded4d46388e241537ae8a2c54bd
Tobias Briones <tbriones@mathswe.com>
2024-09-13 17:28:56 -0600

    Make function match higher-order
    It allows composition with the `pipe` function.

--- src/mathswe-ts/adt.test.ts
+++ src/mathswe-ts/adt.test.ts
@@ -4,16 +4,29 @@
 
 import { describe, expect, it } from "vitest";
 import { match } from "./adt";
+import { pipe } from "fp-ts/function";
 
 describe("SumTypeMap", () => {
-    it("should match sum type variants with type safe exhaustive map", () => {
-        type Shape = { type: "Point" } | { type: "Circle", radius: number };
+    type Shape = { type: "Point" } | { type: "Circle", radius: number };
 
-        const point: Shape = { type: "Point" };
-        const circle: Shape = { type: "Circle", radius: 1 };
+    const point: Shape = { type: "Point" };
+    const circle: Shape = { type: "Circle", radius: 1 };
+
+    it("should match sum type variants with type safe exhaustive map", () => {
         const label = (shape: Shape) => match(
-            shape,
             { "Point": "point-variant", "Circle": "circle-variant" },
+        )(shape);
+
+        expect(label(point)).toBe("point-variant");
+        expect(label(circle)).toBe("circle-variant");
+    });
+
+    it("should pipe match with type safe exhaustive map", () => {
+        const label = (shape: Shape) => pipe(
+            shape,
+            match(
+                { "Point": "point-variant", "Circle": "circle-variant" },
+            ),
         );
 
         expect(label(point)).toBe("point-variant");
