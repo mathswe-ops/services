f310d4753aa658034d7393248f91ed611648f8ee
Tobias Briones <tbriones@mathswe.com>
2024-09-13 17:09:56 -0600

    Add definitions for SumType
    It helps you to create sum types with a safe `match` operation.

--- src/mathswe-ts/adt.test.ts
+++ src/mathswe-ts/adt.test.ts
@@ -1,3 +1,22 @@
 // Copyright (c) 2024 Tobias Briones. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 // This file is part of https://github.com/mathswe-ops/services
+
+import { describe, expect, it } from "vitest";
+import { match } from "./adt";
+
+describe("SumTypeMap", () => {
+    it("should match sum type variants with type safe exhaustive map", () => {
+        type Shape = { type: "Point" } | { type: "Circle", radius: number };
+
+        const point: Shape = { type: "Point" };
+        const circle: Shape = { type: "Circle", radius: 1 };
+        const label = (shape: Shape) => match(
+            shape,
+            { "Point": "point-variant", "Circle": "circle-variant" },
+        );
+
+        expect(label(point)).toBe("point-variant");
+        expect(label(circle)).toBe("circle-variant");
+    });
+});
--- src/mathswe-ts/adt.ts
+++ src/mathswe-ts/adt.ts
@@ -1,3 +1,17 @@
 // Copyright (c) 2024 Tobias Briones. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 // This file is part of https://github.com/mathswe-ops/services
+
+export type SumType = { type: string };
+
+/**
+ * Defines the map type where keys are variant IDs and values are the
+ * corresponding results.
+ */
+export type SumTypeMap<T extends SumType, R> = {
+    [K in T["type"]]: R;
+};
+
+export function match<V extends SumType, R>(value: V, map: SumTypeMap<V, R>): R {
+    return map[value.type as keyof SumTypeMap<V, R>];
+}
