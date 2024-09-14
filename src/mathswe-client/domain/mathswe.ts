2ffcc828664cdc1eac5b65a48c3db1a95d4f8238
Tobias Briones <tbriones@mathswe.com>
2024-09-13 21:46:37 -0600

    Add mod mathswe with MathSwe domains

---
+++ src/mathswe-client/domain/mathswe.ts
@@ -0,0 +1,20 @@
+// Copyright (c) 2024 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/mathswe-ops/services
+
+import { ToDomainName } from "./domain";
+
+export type MathSwe
+    = "MathSweCom"
+    | "MathSoftware"
+    | "MathSoftwareEngineer";
+
+export const mathsweToDomainName: ToDomainName<MathSwe> = {
+    toDomainName(domain: MathSwe): string {
+        return {
+            "MathSweCom": "mathswe.com",
+            "MathSoftware": "mathsoftware.com",
+            "MathSoftwareEngineer": "mathsoftwareengineer.com",
+        }[domain];
+    },
+};
