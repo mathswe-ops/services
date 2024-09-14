5080e24e4a964e2699c68535339c8b74f046285f
Tobias Briones <tbriones@mathswe.com>
2024-09-13 19:03:27 -0600

    Rename field type to tag in SumType
    A sum type is a "tagged union."

--- src/mathswe-ts/adt.ts
+++ src/mathswe-ts/adt.ts
@@ -2,20 +2,20 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // This file is part of https://github.com/mathswe-ops/services
 
-export type SumType = { type: string };
+export type SumType = { tag: string };
 
 /**
  * Defines the map type where keys are variant IDs and values are the
  * corresponding results.
  */
 export type SumTypeMap<T extends SumType, R> = {
-    [K in T["type"]]: R;
+    [K in T["tag"]]: R;
 };
 
 export type MatchValue<V extends SumType, R> = SumTypeMap<V, R>[keyof SumTypeMap<V, R>];
 
 export function match<V extends SumType, R>(map: SumTypeMap<V, R>): (value: V) => MatchValue<V, R> {
-    return (value: V) => map[value.type as keyof SumTypeMap<V, R>];
+    return (value: V) => map[value.tag as keyof SumTypeMap<V, R>];
 }
 
 export function matchVariant<R>(value: SumType): <V>(fn: (variant: V) => R) => R {
