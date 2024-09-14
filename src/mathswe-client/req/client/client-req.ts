17eae4ef91586aa2a282c022d798e9368589e28c
Tobias Briones <tbriones@mathswe.com>
2024-09-14 15:15:33 -0600

    Implement function getOrigin in mod mathswe-client/req/client/client-req

--- src/mathswe-client/req/client/client-req.ts
+++ src/mathswe-client/req/client/client-req.ts
@@ -1,3 +1,14 @@
 // Copyright (c) 2024 Tobias Briones. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 // This file is part of https://github.com/mathswe-ops/services
+
+import { pipe } from "fp-ts/function";
+import * as E from "fp-ts/Either";
+import { Either } from "fp-ts/Either";
+import { newOriginFromString, Origin } from "../origin/origin";
+
+export const getOrigin = (req: Request): Either<string, Origin> => pipe(
+    req.headers.get("Origin"),
+    E.fromNullable("Request header `Origin` is null."),
+    E.flatMap(newOriginFromString),
+);
