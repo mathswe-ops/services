e091699d43772115a2dce2dcd89c37febf271c85
Tobias Briones <tbriones@mathswe.com>
2024-09-14 13:33:08 -0600

    Implement type Origin with SecureUrl and string constructors
    It defines the origin of an incoming HTTP request, so the server can reject it if unaccepted.

--- src/mathswe-client/origin/origin.ts
+++ src/mathswe-client/origin/origin.ts
@@ -1,3 +1,29 @@
 // Copyright (c) 2024 Tobias Briones. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 // This file is part of https://github.com/mathswe-ops/services
+
+import { OriginDomain, originDomainFromUrl } from "./origin-domain";
+import { pipe } from "fp-ts/function";
+import { newUrlFromString, SecureUrl } from "../req/http";
+import * as E from "fp-ts/Either";
+import { Either } from "fp-ts/Either";
+
+export type Origin = {
+    domain: OriginDomain,
+    url: SecureUrl,
+}
+
+export const newOriginFromUrl = (url: SecureUrl): Either<string, Origin> => pipe(
+    url,
+    originDomainFromUrl,
+    E.map(domain => ({
+        domain,
+        url,
+    })),
+);
+
+export const newOriginFromString = (origin: string): Either<string, Origin> => pipe(
+    origin,
+    newUrlFromString,
+    E.flatMap(newOriginFromUrl),
+);
