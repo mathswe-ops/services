dc9de3272d6ff3f6515b99670e220d9c790f871a
Tobias Briones <tbriones@mathswe.com>
2024-09-15 00:56:42 -0600

    Make match sum type lazy
    It fixes the erroneous (eager) execution of all the map branches automatically when it only must execute (lazily) the matching branch.

--- src/mathswe-ts/adt.ts
+++ src/mathswe-ts/adt.ts
@@ -2,22 +2,22 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // This file is part of https://github.com/mathswe-ops/services
 
+import { LazyArg } from "fp-ts/function";
+
 export type SumType = { tag: string };
 
 /**
- * Defines the map type where keys are variant IDs and values are the
+ * Defines the lazy map type where keys are variant IDs and values are the
  * corresponding results.
  */
 export type SumTypeMap<T extends SumType, R> = {
-    [K in T["tag"]]: R;
+    [K in T["tag"]]: LazyArg<R>;
 };
 
-export type MatchValue<V extends SumType, R> = SumTypeMap<V, R>[keyof SumTypeMap<V, R>];
-
-export function match<V extends SumType, R>(map: SumTypeMap<V, R>): (value: V) => MatchValue<V, R> {
-    return (value: V) => map[value.tag as keyof SumTypeMap<V, R>];
+export function match<V extends SumType, R>(map: SumTypeMap<V, R>): (value: V) => R {
+    return (value: V) => map[value.tag as keyof SumTypeMap<V, R>]();
 }
 
-export function withMatchVariant<R>(value: SumType): <V>(fn: (variant: V) => R) => R {
-    return <V>(fn: (variant: V) => R) => fn(value as V);
+export function withMatchVariant<R>(value: SumType): <V>(fn: (variant: V) => R) => LazyArg<R> {
+    return <V>(fn: (variant: V) => R) => () => fn(value as V);
 }
