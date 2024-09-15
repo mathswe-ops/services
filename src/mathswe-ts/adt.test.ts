607e4a9d3aac5f9b8f564e73d25cc22a1dc2d033
Tobias Briones <tbriones@mathswe.com>
2024-09-15 00:56:42 -0600

    Make match sum type lazy
    It fixes the erroneous (eager) execution of all the map branches automatically when it only must execute (lazily) the matching branch.

--- src/mathswe-ts/adt.test.ts
+++ src/mathswe-ts/adt.test.ts
@@ -5,35 +5,33 @@
 import { describe, expect, it } from "vitest";
 import { match, withMatchVariant } from "./adt";
 import { pipe } from "fp-ts/function";
+import * as O from "fp-ts/Option";
 
-describe("SumTypeMap", () => {
+describe("Lazy Plain SumType", () => {
     type Shape = { tag: "Point" } | { tag: "Circle", radius: number };
 
     const point: Shape = { tag: "Point" };
     const circle: Shape = { tag: "Circle", radius: 1 };
 
-    it("should match sum type variants with type safe exhaustive map", () => {
-        const label = (shape: Shape) => match({
-            Point: "point-variant",
-            Circle: "circle-variant",
-        })(shape);
-
-        expect(label(point)).toBe("point-variant");
-        expect(label(circle)).toBe("circle-variant");
-    });
-
-    it("should pipe match with type safe exhaustive map", () => {
+    it("should pipe lazy match with type safe exhaustive map", () => {
         const label = (shape: Shape) => pipe(
             shape,
             match({
-                Point: "point-variant",
-                Circle: "circle-variant",
+                Point: () => "point-variant",
+                Circle: () => "circle-variant",
             }),
         );
 
         expect(label(point)).toBe("point-variant");
         expect(label(circle)).toBe("circle-variant");
     });
+});
+
+describe("Lazy Rich SumType", () => {
+    type Shape = { tag: "Point" } | { tag: "Circle", radius: number };
+
+    const point: Shape = { tag: "Point" };
+    const circle: Shape = { tag: "Circle", radius: 1 };
 
     it("computes the area via matching and destructure", () => {
         type Circle = { radius: number };
@@ -47,7 +45,7 @@ describe("SumTypeMap", () => {
             return pipe(
                 shape,
                 match({
-                    Point: 0,
+                    Point: () => 0,
                     Circle: withShapeVariant(circleArea),
                 }),
             );
@@ -55,5 +53,99 @@ describe("SumTypeMap", () => {
 
         expect(area(point)).toBe(0);
         expect(area(circle)).toBe(Math.PI);
+    });
+
+    it("computes only the required branch", () => {
+        type Circle = { radius: number };
+
+        let executionTimes = 0;
+
+        const circleArea = ({ radius }: Circle) => {
+            executionTimes++;
+
+            return Math.PI * Math.pow(radius, 2);
+        };
+
+        const pointArea = () => {
+            executionTimes++;
+
+            return 0;
+        };
+
+        const area = (shape: Shape): number => {
+            const withShapeVariant = withMatchVariant<number>(shape);
+
+            return pipe(
+                shape,
+                match({
+                    Point: withShapeVariant(pointArea),
+                    Circle: withShapeVariant(circleArea),
+                }),
+            );
+        };
+
+        expect(executionTimes).toBe(0);
+
+        expect(area(point)).toBe(0);
+        expect(executionTimes).toBe(1);
+
+        expect(area(circle)).toBe(Math.PI);
+        expect(executionTimes).toBe(2);
+    });
+
+    it("computes the area lazily", () => {
+        // Lazy matching is crucial to avoid executing unsafe code eagerly.
+        // e.g., eager runs code for non-matching branches automatically.
+
+        // With the initial eager match implementation, it fails with
+        // `TypeError: Cannot read properties of undefined (reading 'find')`
+        // because it calls the `PartialAccess` branch automatically (with no
+        // arguments).
+
+        type Allowed
+            = { tag: "FullAccess" }
+            | { tag: "PartialAccess", paths: string[] };
+
+        type PartialAccess = { paths: string[] };
+
+        const findBasePath = (path: string) => (allowedPaths: string[]) =>
+            allowedPaths.find(allowedPath => path.startsWith(allowedPath));
+
+        // `{ paths }: PartialAccess` param type defines the argument of the
+        // `PartialAccess` variant unlike `FullAccess` that has no fields.
+
+        const belongsToPartialAccess
+            = (path: string) => ({ paths }: PartialAccess): boolean => pipe(
+            paths,
+            findBasePath(path),
+            O.fromNullable,
+            O.isSome,
+        );
+
+        const isPathAllowed = (path: string, allowed: Allowed): boolean => {
+            const withAllowedVariant = withMatchVariant<boolean>(allowed);
+
+            return pipe(
+                allowed,
+                match({
+                    FullAccess: () => true,
+                    PartialAccess:
+                        withAllowedVariant(belongsToPartialAccess(path)),
+                }),
+            );
+        };
+
+        const adminPath = "users/admin";
+        const fullAccess: Allowed = { tag: "FullAccess" };
+        const partialAccess: Allowed = {
+            tag: "PartialAccess",
+            paths: ["public", "users/guest"],
+        };
+
+        const isFullAccessAllowed = isPathAllowed(adminPath, fullAccess);
+        const isPartialAccessAllowed = isPathAllowed(adminPath, partialAccess);
+
+        expect(isFullAccessAllowed).toBe(true);
+        expect(isPartialAccessAllowed).toBe(false);
     });
 });
