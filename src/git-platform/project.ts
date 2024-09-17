aa7befd294104d45d4404017f846233895f219bf
Tobias Briones <tbriones@mathswe.com>
2024-09-17 13:23:55 -0600

    Implement inference of project version from repo URL (Npm, Cargo)
    It provides a method that finds the project version from its repository URL. It supports the current `BuildSystem` projects, `package.json` and `Cargo.toml`. The branch name must be `main` to read the production files.

--- src/git-platform/project.ts
+++ src/git-platform/project.ts
@@ -2,18 +2,29 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // This file is part of https://github.com/mathswe-ops/services
 
-import { GitPlatform } from "./git-platform";
+import { GitPlatform, gitPlatformFromUrl } from "./git-platform";
+import * as toml from "@iarna/toml";
+import * as O from "fp-ts/Option";
 import { none, Option, some } from "fp-ts/Option";
 import { pipe } from "fp-ts/function";
 import * as E from "fp-ts/Either";
 import { Either, left, right } from "fp-ts/Either";
 import { matchPlain } from "../mathswe-ts/enum";
+import { requireRight } from "../mathswe-ts/require";
 
 export type BuildSystem = { tag: "Npm" } | { tag: "Cargo" };
 
 export const npm: BuildSystem = { tag: "Npm" };
 
 export const cargo: BuildSystem = { tag: "Cargo" };
+
+export const buildFile = (system: BuildSystem): string => pipe(
+    system,
+    matchPlain({
+        Npm: "package.json",
+        Cargo: "Cargo.toml",
+    }),
+);
 
 export function readBuildSystem(
     gitPlatform: GitPlatform,
@@ -25,6 +36,22 @@ export function readBuildSystem(
             GitHub: readBuildSystemOnGitHub(url),
         }),
     );
+}
+
+export async function inferVersion(
+    repoUrl: string,
+): Promise<Either<string, string>> {
+    const gitPlatform = gitPlatformFromUrl(repoUrl);
+    let result;
+
+    if (E.isRight(gitPlatform)) {
+        result = await inferVersionFrom(gitPlatform.right, repoUrl);
+    }
+    else {
+        result = gitPlatform;
+    }
+
+    return result;
 }
 
 type GitHubRepoContent = {
@@ -100,6 +127,12 @@ async function fetchFileListOnGitHub(
         result = pipe(
             okResult,
             E.map(_ => json),
+            E.flatMap(data =>
+                Array.isArray(data)
+                ? right(data)
+                : left("Fail to read project files. Ensure to provide the"
+                       + " correct repository URL"),
+            ),
         );
     }
     catch (error) {
@@ -107,4 +140,151 @@ async function fetchFileListOnGitHub(
     }
 
     return result;
+}
+
+async function inferVersionFrom(
+    gitPlatform: GitPlatform,
+    repoUrl: string,
+): Promise<Either<string, string>> {
+    const systemResult = await readBuildSystem(gitPlatform, repoUrl);
+
+    const systemFound = pipe(
+        systemResult,
+        E.flatMap(
+            O.match(
+                () => left(`Fail to find a build system on ${ repoUrl }`),
+                system => right(system),
+            ),
+        ),
+    );
+
+    let result;
+
+    if (E.isRight(systemFound)) {
+        const system = pipe(systemFound, requireRight);
+
+        result = await readProjectVersion(gitPlatform, repoUrl, system);
+    }
+    else {
+        result = systemFound;
+    }
+
+    return result;
+}
+
+async function readProjectVersion(
+    gitPlatform: GitPlatform,
+    repoUrl: string,
+    system: BuildSystem,
+): Promise<Either<string, string>> {
+    return pipe(
+        gitPlatform,
+        matchPlain({
+            GitHub: readProjectVersionOnGitHub(repoUrl, system),
+        }),
+    );
+}
+
+async function readProjectVersionOnGitHub(
+    repoUrl: string,
+    system: BuildSystem,
+): Promise<Either<string, string>> {
+    const filename = buildFile(system);
+    const fileResult = await readFileOnGitHub(repoUrl, "main", filename);
+
+    return pipe(
+        fileResult,
+        E.flatMap(fileContent => readVersionFromBuildFile(system, fileContent)),
+    );
+}
+
+async function readFileOnGitHub(
+    repoUrl: string,
+    branch: string,
+    filePath: string,
+): Promise<Either<string, string>> {
+    const apiUrl = repoUrl
+        .replace("https://github.com/", "https://raw.githubusercontent.com/")
+        .concat(`/${ branch }/${ filePath }`);
+
+    const init = {
+        headers: {
+            Accept: "application/vnd.github.raw+json",
+        },
+    };
+
+    let result;
+
+    try {
+        const response = await fetch(apiUrl, init);
+        const okResult
+            = response.ok
+              ? right(response)
+              : left(`Fail to fetch ${apiUrl}. GitHub User Content error: ${ response.statusText }`);
+
+        const text = E.isRight(okResult) ? await response.text() : "";
+
+        result = pipe(
+            okResult,
+            E.map(_ => text),
+        );
+    }
+    catch (error) {
+        result = left(`Failed to fetch file ${ filePath }: ${ (error as Error).message }`);
+    }
+
+    return result;
+}
+
+function readVersionFromBuildFile(
+    system: BuildSystem,
+    fileContent: string,
+): Either<string, string> {
+    return pipe(
+        system,
+        matchPlain({
+            Npm: readVersionFromPackageJson(fileContent),
+            Cargo: readVersionFromCargoToml(fileContent),
+        }),
+    );
+}
+
+function readVersionFromPackageJson(
+    packageJson: string,
+): Either<string, string> {
+    let packageResult: Either<string, Record<string, string | undefined>>;
+
+    try {
+        const parse = JSON.parse(packageJson);
+        packageResult = right(parse);
+    }
+    catch (error) {
+        packageResult = left(`Fail to parse package.json: ${ error }`);
+    }
+
+    return pipe(
+        packageResult,
+        E.map(data => data["version"]),
+        E.flatMap(E.fromNullable("Version field not found in package.json")),
+    );
+}
+
+function readVersionFromCargoToml(
+    cargoToml: string,
+): Either<string, string> {
+    let cargoResult: Either<string, Record<string, any>>;
+
+    try {
+        const parsed = toml.parse(cargoToml);
+        cargoResult = right(parsed);
+    }
+    catch (error) {
+        cargoResult = left(`Failed to parse Cargo.toml: ${ error }`);
+    }
+
+    return pipe(
+        cargoResult,
+        E.map(data => data["package"]?.["version"]),
+        E.flatMap(E.fromNullable("Version field not found in Cargo.toml")),
+    );
 }
